5a5f291e4d007f3058d89ca73090bc42ab2d0e2b
Adrian Adegbesan <adrianadegbesan@gmail.com>
2025-02-14 14:54:25 -0800

    Discord Bot Skeleton Done

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
+import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
+# Load the token from .env
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Set up bot with a command prefix
 intents = discord.Intents.default()
 intents.message_content = True
+intents.messages = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f"We have logged in as {bot.user}")
 
-bot.run('YOUR_BOT_TOKEN')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello! Your bot is working!")
+
+
+bot.run(TOKEN)
